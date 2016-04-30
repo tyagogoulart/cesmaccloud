996de128dc5c771058e2a8bf9c64e030c03c9e0d
iigorc <igorcavalcante02@gmail.com>
2016-04-30 14:56:05 -0300

    Implementação de teste de método POST: criação de todo.

---
+++ todos/views/teste_todos.py
@@ -0,0 +1,16 @@
+from django.core.urlresolvers import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from todos.models.todo import Todo
+from django.contrib.auth.models import User
+from todos.views.TodoSerializer import TodoSerializer
+
+class TesteCriarTodo(APITestCase):
+    def setUp(self):
+        self.data = {'descricao': 'teste', 'data': '30/04/2016'}
+        
+    def teste_criar_todo(self):
+        response = self.client.post('/todos/', self.data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Todo.objects.count(), 1)
+        self.assertEqual(Todo.objects.get().descricao, 'teste')
