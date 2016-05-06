891f9859de3f77a295aa1fd14e4275427a03b92f
Tyago Goulart <tyagogoulartn@gmail.com>
2016-05-06 20:18:23 -0300

    Documentação de código.

--- todos/models/todo.py
+++ todos/models/todo.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 class Todo(models.Model):
+    """
+    Modelo que define os campos da tabela todos no banco de dados
+    e que serão utilizados na API.
+    """
     descricao = models.CharField(max_length=800)
     data = models.CharField(max_length=10)
     
